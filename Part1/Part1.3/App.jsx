c4eae6032b686c719ebd5247d3efcd5d16b2b9eb
demoanon <151359938+demoanon@users.noreply.github.com>
2024-07-20 13:37:40 +0530

    Add files via upload

---
+++ Part1/Part1.3/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
+function App() {
+
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1, part2, part3]} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  );
+}
+
+export default App;
+
